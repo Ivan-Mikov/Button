8bb710d6b02ae4916a06a77fa30e12a1f431cce6
m1ster-king <ivanmikov2006@gmail.com>
2025-09-20 21:24:52 +0300

    Add variations of buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import Button from "./components/Button/Button";
+import "./index.scss";
 
 export default function App() {
-  return <Button text="Text" />;
+  return (
+    <div className="btn-div">
+      <Button text="Primary" />
+      <Button text="Secondary" style="secondary" />
+      <Button text="Danger" style="danger" />
+      <Button text="Default" style="default" />
+      <Button text="Primary" size="small" />
+      <Button text="Secondary" style="secondary" size="small" />
+      <Button text="Danger" style="danger" size="small" />
+      <Button text="Default" style="default" size="small" />
+    </div>
+  );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,18 @@
 import "./Button.scss";
 
+type style = "primary" | "secondary" | "danger" | "default";
+type size = "small" | "normal";
+
 interface ButtonProps {
   text: string;
+  style?: style;
+  size?: size;
 }
 
-export default function Button({ text }: ButtonProps) {
-  return <button>{text}</button>;
+export default function Button({
+  text,
+  style = "primary",
+  size = "normal",
+}: ButtonProps) {
+  return <button className={`btn btn-${style} btn-${size}`}>{text}</button>;
 }
